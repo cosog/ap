92c0f65937a512c5220d36e3f6429713e028726b
Administrator <Administrator@DESKTOP-DR515C1>
2021-08-17 20:28:50 +0800

    test

--- src/com/cosog/task/EquipmentDriverServerTask.java
+++ src/com/cosog/task/EquipmentDriverServerTask.java
@@ -49,7 +49,7 @@ public class EquipmentDriverServerTask {
 		return instance;
 	}
 	
-	@Scheduled(fixedRate = 1000*60*60*24*365*100)
+//	@Scheduled(fixedRate = 1000*60*60*24*365*100)
 	public void driveServerTast() throws SQLException, ParseException,InterruptedException, IOException{
 		Gson gson = new Gson();
 		java.lang.reflect.Type type=null;

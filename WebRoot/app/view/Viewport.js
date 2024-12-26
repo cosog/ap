728c6aeec981f5e84016ef01f2fe5e1f4e959dc4
cosog-zhaojm <271875319@qq.com>
2024-12-26 18:42:10 +0800

    bug fix

--- WebRoot/app/view/Viewport.js
+++ WebRoot/app/view/Viewport.js
@@ -325,8 +325,12 @@ function websocketOnMessage(evt) {
                         }
                     } else if (activeId == "RealTimeMonitoringFSDiagramAnalysisTabPanel_Id") {
                         //井筒分析
-                        showFSDiagramFromPumpcard(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv1_id"); // 调用画泵功图的函数
-                        showRodPress(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv2_id"); // 调用画杆柱应力的函数
+                    	if(data.wellBoreChartsData.resultCode==1232){
+                    		showSurfaceCard(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv1_id");
+                    	}else{
+                    		showFSDiagramFromPumpcard(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv1_id"); // 调用画泵功图的函数
+                    	}
+                    	showRodPress(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv2_id"); // 调用画杆柱应力的函数
                         showPumpCard(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv3_id"); // 调用画泵功图的函数
                         showPumpEfficiency(data.wellBoreChartsData, "FSDiagramAnalysisSingleWellboreDetailsDiv4_id"); // 调用画泵效组成的函数
                     } else if (activeId == "RealTimeMonitoringFSDiagramAnalysisSurfaceTabPanel_Id") {
